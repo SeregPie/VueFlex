78d6c81af0a52eddbe26630576b60623efeec6cd
Sergej Sintschilin <seregpie@gmail.com>
2018-07-04 16:53:46 +0200

    +

--- demo/index.js
+++ demo/index.js
@@ -1,24 +1,16 @@
 (function() {
 
 	new Vue({
-		el: '#App',
+		el: '#app',
 
 		data: {
-			input: {
-				spacing: {
-					values: [0, 1, 10, 50],
-					valueIndex: 2,
-				},
-			},
+			spacingValues: [0, 1, 10, 50],
+			spacingValueIndex: 2,
 		},
 
 		computed: {
 			spacing: function() {
-				var values = this.input.spacing.values;
-				var valueIndex = this.input.spacing.valueIndex;
-
-				var value = values[valueIndex];
-				return value;
+				return this.spacingValues[this.spacingValueIndex];
 			},
 		},
 	});
